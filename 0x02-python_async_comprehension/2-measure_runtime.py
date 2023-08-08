7295237a19c49dbc423233289c2afc4c95d53041
Kidus Gizaw <50879166+KidusGR@users.noreply.github.com>
2023-08-08 21:52:56 +0300

    0x02-python_async_comprehension

---
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+""" Measure times """
+import asyncio
+import time
+
+async_comprehension = __import__('1-async_comprehension').async_comprehension
+
+
+async def measure_runtime() -> float:
+    """
+        measure time and execute in paralallel
+
+        Arguments:
+            void
+
+        Returns:
+            float random numbers
+    """
+
+    first_time = time.perf_counter()
+    tasks = [async_comprehension() for _ in range(4)]
+    await asyncio.gather(*tasks)
+    elapsed = time.perf_counter()
+
+    return (elapsed - first_time)
